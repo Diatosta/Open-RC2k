d25623e7b83eef11f1699b4540953ee93cecd57d
DIATOSTA-PC\diato <diatosta123@gmail.com>
2023-08-08 12:30:23 +0100

    Added ability to run code other than the bypass

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,17 @@
+use std::{mem, ptr, thread};
 use windows::{
     core::PCSTR,
+    imp::{GetProcAddress, LoadLibraryA},
     Win32::{
-        Foundation::{BOOL, HANDLE, HWND},
+        Foundation::{BOOL, HMODULE, HWND},
+        System::LibraryLoader::GetModuleHandleA,
         UI::WindowsAndMessaging::{MessageBoxA, MESSAGEBOX_STYLE},
     },
 };
 
-#[no_mangle]
-extern "C" fn DirectInputCreateA() {
+const SUB401EDE: usize = 0x401EDE;
+
+fn inject_stuff() {
     unsafe {
         MessageBoxA(
             HWND(0),
@@ -15,5 +19,43 @@ extern "C" fn DirectInputCreateA() {
             PCSTR("Uh oh\x00".as_ptr()),
             MESSAGEBOX_STYLE(0),
         );
+
+        let module_handle = GetModuleHandleA(PCSTR(ptr::null())).unwrap();
+        let handle = format!("module_handle: {:x}\x00", module_handle.0);
+        MessageBoxA(
+            HWND(0),
+            PCSTR(handle.as_ptr()),
+            PCSTR("Uh oh\x00".as_ptr()),
+            MESSAGEBOX_STYLE(0),
+        );
+    };
+}
+
+#[no_mangle]
+extern "C" fn DirectInputCreateA() -> u32 {
+    unsafe {
+        let original_dll = LoadLibraryA(PCSTR("C:\\WINDOWS\\SysWOW64\\DINPUT.dll\x00".as_ptr()));
+        if original_dll == 0 {
+            return 0;
+        }
+
+        let original_function: extern "C" fn() -> u32 = mem::transmute(GetProcAddress(
+            original_dll,
+            PCSTR("DirectInputCreateA\x00".as_ptr()),
+        ));
+
+        original_function()
     }
+}
+
+#[no_mangle]
+extern "system" fn DllMain(_module_handle: HMODULE, dw_reason: u32, _lp_reserved: &u32) -> BOOL {
+    match dw_reason {
+        1u32 => {
+            std::thread::spawn(inject_stuff);
+        }
+        _ => return BOOL(0),
+    };
+
+    BOOL(1)
 }
